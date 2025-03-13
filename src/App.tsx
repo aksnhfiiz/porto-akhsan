0cbe01cad146532a658064b1095038b6a28e74e5
aksnhfiiz <akhsanhafizh1@gmail.com>
2025-03-13 23:28:51 +0700

    bismillah

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-// src/App.tsx
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from './components/navbar';
-import Home from './pages/home';
-import Pendidikan from './pages/pendidikan';
-import Skill from './pages/skill';
-import Pengalaman from './pages/experience';
-import Contact from './pages/contact';
-import About from './pages/about';
-import Proyek from './pages/proyek';
+import Navbar from "./components/navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Pendidikan from "./pages/pendidikan";
+import Skill from "./pages/skill";
+import Pengalaman from "./pages/experience";
+import Contact from "./pages/contact";
+import About from "./pages/about";
+import Proyek from "./pages/proyek";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pendidikan" element={<Pendidikan />} />
-          <Route path="/skill" element={<Skill />} />
-          <Route path="/pengalaman" element={<Pengalaman />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/proyek" element={<Proyek />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pendidikan" element={<Pendidikan />} />
+        <Route path="/skill" element={<Skill />} />
+        <Route path="/pengalaman" element={<Pengalaman />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/proyek" element={<Proyek />} />
+      </Routes>
+    </div>
   );
 }
 
 export default App;
+
